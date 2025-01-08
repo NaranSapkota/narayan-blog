ff52453d21eed17f6208ff7498e4cb1485e16a02
Narayan Sapkota <xanzong.naran29@gmail.com>
2025-01-08 14:48:56 -0500

    IMplemantaing the Home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title='Welcome to the new blog';
-  const like=50;
+  
 
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {like } times</p>
-        <p>{34}</p>
+        <Home/>
       </div>
     </div>
   );
