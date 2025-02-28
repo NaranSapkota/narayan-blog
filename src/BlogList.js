c6703854c2d43e4eb03d35d54af1143a46bd8ed3
Narayan Sapkota <xanzong.naran29@gmail.com>
2025-02-27 20:02:28 -0500

    Implementating delete

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({blogs,title}) => {
+const BlogList = ({blogs,title,handleDelete}) => {
     
     return ( 
         <div className="blog-list">
@@ -6,8 +6,10 @@ const BlogList = ({blogs,title}) => {
  {
             blogs.map((blog)=>(
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
+                    <h2 style={{ color: 'red' }}>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={()=> handleDelete(blog.id)}>delete blog</button>
+
                 </div>
             ))
           }
--- src/Home.js
+++ src/Home.js
@@ -27,10 +27,16 @@ const Home = () => {
         ]
     );
 
+    const handleDelete=(id)=>{
+      const newBlogs=blogs.filter(blog=> blog.id != id);
+      setBlog(newBlogs);
+    }
+
     return ( 
         <div className="home">
-         <BlogList blogs={blogs} title="All Blogs"/>
-         <BlogList blogs={blogs.filter((blog)=> blog.author==="Three")} title={'Author Three Blog'}/>
+         <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      
+         {/* <BlogList blogs={blogs.filter((blog)=> blog.author==="Three")} title={'Author Three Blog'}/> */}
           
         </div>
      );
