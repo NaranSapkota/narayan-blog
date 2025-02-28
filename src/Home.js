c0b083d77df568137339021a8bc16fa92088fdb5
Narayan Sapkota <xanzong.naran29@gmail.com>
2025-02-27 20:12:12 -0500

    useEffect Hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -31,7 +31,10 @@ const Home = () => {
       const newBlogs=blogs.filter(blog=> blog.id != id);
       setBlog(newBlogs);
     }
-
+useEffect(()=>{
+  console.log('use effect ran');
+  console.log(blogs);
+});
     return ( 
         <div className="home">
          <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
