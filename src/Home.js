444b6381bfd300b1981e12a0aa7144441d680243
Narayan Sapkota <xanzong.naran29@gmail.com>
2025-03-07 14:44:17 -0500

    Making custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,46 +1,26 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlog] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error,setError]=useState(null);
+
+  // const [name, setName] = useState('mario');
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter(blog => blog.id != id);
+  //   setBlog(newBlogs);
+  // }
 
 
-  const [name, setName] = useState('mario');
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id != id);
-    setBlog(newBlogs);
-  }
 
+  const {data :blogs,isPending,error}=useFetch('http://localhost:8000/blogs');
 
-  useEffect(() => {
-
-    setTimeout(() => {
-
-      fetch('http://localhost:8000/blogs')
-        .then(res => {
-          if(!res.ok){
-            throw Error('Could not load fetch data for that resources');
-          }
-          return res.json();
-        }).then(data => {
-          setBlog(data)
-          setIsPending(false);
-        }).catch(err=>{
-          setError(err.message);
-          setIsPending(false);
-        })
-    }, 1000);
-
-  }, []);
 
 
   return (
     <div className="home">
       { error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
+      {blogs && <BlogList blogs={blogs} title="All Blogs"  />}
 
       
       {/* <button onClick={() => setName('luigi')}>change name</button> */}
