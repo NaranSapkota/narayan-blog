3f3b34c26c96ad8d79381d1b381e9867d012a032
Narayan Sapkota <xanzong.naran29@gmail.com>
2025-03-07 15:11:23 -0500

    Set Path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 
 function App() {
@@ -13,9 +14,15 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+
+            <Route exact path="/">
               <Home />
             </Route>
+
+            <Route path="/create">
+              <Create />
+            </Route>
+
           </Switch>
 
         </div>
---
+++ src/Create.js
@@ -0,0 +1,10 @@
+const Create = () => {
+    return ( 
+        <div className="create">
+            <h2>Add a new blogs</h2>
+
+        </div>
+     );
+}
+ 
+export default Create;
